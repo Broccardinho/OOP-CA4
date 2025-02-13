6801ca8df8513432d8311c8e4ec20d63513b1b93
AlexBroccardo <159144351+Broccardinho@users.noreply.github.com>
2025-02-13 14:27:50 +0000

    build(project): update
    built constructors

--- src/main/java/database/databaseConnection.java
+++ src/main/java/database/databaseConnection.java
@@ -3,11 +3,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Database connection class handles my database connection setup.
+ */
 public class databaseConnection {
+    //Database URL, Username, Password
     private static final String URL = "jdbc:mysql://localhost:3306/income_expense_tracker";
     private static final String USER = "root";
     private static final String PASSWORD = "";
-
+/**
+ * Establishes and returns a connection to the database.
+ * @throws SQLException if teh connection fails
+ */
     public static Connection getConnection()throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
--- src/main/java/dto/Income.java
+++ src/main/java/dto/Income.java
@@ -1,4 +1,58 @@
 package dto;
+import java.util.Date;
 
 public class Income {
+    private int incomeID;
+    private String title;
+    private double amount;
+    private Date dateEarned;
+
+    public Income(int incomeID, String title, double amount, Date dateEarned) {
+        this.incomeID = incomeID;
+        this.title = title;
+        this.amount = amount;
+        this.dateEarned = dateEarned;
+    }
+
+    public int getIncomeID() {
+        return incomeID;
+    }
+
+    public void setIncomeID(int incomeID) {
+        this.incomeID = incomeID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDateEarned() {
+        return dateEarned;
+    }
+
+    public void setDateEarned(Date dateEarned) {
+        this.dateEarned = dateEarned;
+    }
+
+    @Override
+    public String toString() {
+        return "Income{" +
+                "incomeID=" + incomeID +
+                ", title='" + title + '\'' +
+                ", amount=" + amount +
+                ", dateEarned=" + dateEarned +
+                '}';
+    }
 }
