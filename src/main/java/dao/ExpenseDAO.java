25f52aa15be62458c97e2324b8a4f5bc496a1ed7
AlexBroccardo <159144351+Broccardinho@users.noreply.github.com>
2025-02-14 09:23:47 +0000

    build(project): update
    built ExpenseDAO

--- src/main/java/dao/ExpenseDAO.java
+++ src/main/java/dao/ExpenseDAO.java
@@ -1,4 +1,58 @@
 package dao;
+import database.databaseConnection;
+import dto.Expense;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExpenseDAO {
+
+
+    public List<Expense> getAllExpenses() {
+        List<Expense> expenses = new ArrayList<>();
+        String query = "SELECT * FROM expense";
+
+        try(Connection connection = databaseConnection.getConnection();
+            PreparedStatement prepState = connection.prepareStatement(query())){
+            ResultSet resSet = prepState.executeQuery();
+
+            while(resSet.next()){
+                expenses.add(new Expense(
+                        resSet.getInt("expenseID"),
+                        resSet.getString("title"),
+                        resSet.getString("category"),
+                        resSet.getDouble("amount"),
+                        resSet.getDate("dateIncurred")
+                ));
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return expenses;
+    }
+    public void addExpense(String title, String category, double amount, Date dateIncurred){
+        String query = "INSERT INTO expense (title, category, amount, dateIncurred) VALUES (?, ?, ?, ?)";
+
+        try(Connection connection = databaseConnection.getConnection();
+        PreparedStatement prepState = connection.prepareStatement(query)){
+            prepState.setString(1, title);
+            prepState.setString(2, category);
+            prepState.setDouble(3, amount);
+            prepState.setDate(4, dateIncurred);
+            prepState.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void deleteExpense(int expenseID){
+        String query = "DELETE FROM expense WHERE expenseID = ?";
+
+        try(Connection connection = databaseConnection.getConnection();
+        PreparedStatement prepState = connection.prepareStatement(query)){
+            prepState.setInt(1, expenseID);
+            prepState.executeUpdate();
+    }catch (SQLException e){
+        e.printStackTrace();}
+    }
 }
